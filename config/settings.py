2f2d21bd07e95825d4fd3f6125403580a0617323
LIRIKO <cris.liriko@gmail.com>
2026-02-06 19:42:02 -0400

    modificando pipipi

--- config/settings.py
+++ config/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'saltenieria_db',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
         'USER': 'root', 
         'PASSWORD': '123456',
         'HOST': '127.0.0.1',
