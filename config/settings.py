60b7df1992cc267089a3115f1c9b997406f9f7da
LIRIKO <cris.liriko@gmail.com>
2026-02-06 19:48:06 -0400

    modificando pipipi222

--- config/settings.py
+++ config/settings.py
@@ -78,10 +78,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'USER': 'root', 
-        'PASSWORD': '123456',
-        'HOST': '127.0.0.1',
-        'PORT': '3307',
     }
 }
 
